2876b4b41394bdc5307ef2252d64b43aeb3d3523
kammachat <kammachat.k@ku.th>
2022-05-09 17:11:46 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <div class="upload">
+      <form action="/index.html" method="post" enctype="multipart/form">
+        <v-file-input label="File input" filled prepend-icon="mdi-camera" @change="onFileSelected"/>
+        <v-btn depressed color="primary" @click="onSubmitFile"> Submit </v-btn>
+      </form>
+    </div>
+  </div>
+</template>
