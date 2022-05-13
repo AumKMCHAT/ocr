29380dcd092f607ade9386b99b2178172f024f55
kammachat <kammachat.k@ku.th>
2022-05-13 10:17:45 +0700

    12cec5e

---
+++ _nuxt/static/1652411781/inspire/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"ocr\u002F_nuxt\u002Fstatic\u002F1652411781",layout:"default",error:a,serverRendered:true,routePath:"\u002Finspire",config:{_app:{basePath:"ocr\u002F",assetsPath:"ocr\u002F_nuxt\u002F",cdnURL:a}}}}(null));
