e845e69ac71a271acaf8a2bbbb4e1ee3675d0b14
kammachat <kammachat.k@ku.th>
2022-05-13 10:05:26 +0700

    12867a9

---
+++ _nuxt/static/1652410962/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"ocr\u002F_nuxt\u002Fstatic\u002F1652410962",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"ocr\u002F",assetsPath:"ocr\u002F_nuxt\u002F",cdnURL:a}}}}(null));
